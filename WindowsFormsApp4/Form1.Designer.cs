698e3559d28e748466f118012609664b4a0c4f5e
Динара с <dinartas1@gmail.com>
2021-03-23 00:12:58 +0500

    без MVC

--- WindowsFormsApp4/Form1.Designer.cs
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -31,16 +31,16 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.progressBar2 = new System.Windows.Forms.ProgressBar();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,7 +50,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(151, 26);
             this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox1_KeyDown);
             // 
             // label1
             // 
@@ -75,6 +75,19 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
             // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(71, 56);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.NumericUpDown1_ValueChanged);
+            // 
             // progressBar1
             // 
             this.progressBar1.Location = new System.Drawing.Point(61, 118);
@@ -95,6 +108,19 @@
             this.groupBox2.TabIndex = 4;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "groupBox2";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(72, 56);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown2.TabIndex = 6;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.NumericUpDown2_ValueChanged);
             // 
             // progressBar2
             // 
@@ -120,23 +146,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(151, 26);
             this.textBox2.TabIndex = 1;
-            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
-            // 
-            // numericUpDown1
-            // 
-            this.numericUpDown1.Location = new System.Drawing.Point(71, 56);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 26);
-            this.numericUpDown1.TabIndex = 5;
-            this.numericUpDown1.ValueChanged += new System.EventHandler(this.NumericUpDown1_ValueChanged);
-            // 
-            // numericUpDown2
-            // 
-            this.numericUpDown2.Location = new System.Drawing.Point(72, 56);
-            this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(120, 26);
-            this.numericUpDown2.TabIndex = 6;
-            this.numericUpDown2.ValueChanged += new System.EventHandler(this.NumericUpDown2_ValueChanged);
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox2_KeyDown);
             // 
             // Form1
             // 
@@ -149,9 +159,9 @@
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.ResumeLayout(false);
 
--- WindowsFormsApp4/Form1.cs
+++ WindowsFormsApp4/Form1.cs
@@ -37,21 +37,21 @@ namespace WindowsFormsApp4
 
 
         private void ProgressBar1_Click(object sender, EventArgs e)
-        {
+        {/*
             if (!(progressBar1.Value > progressBar2.Value))
             {
-                progressBar2.Value = Decimal.ToInt32(progressBar1.Value-1);
+                progressBar2.Value =progressBar1.Value-1;
                 numericUpDown2.Value = progressBar2.Value;
                 textBox2.Text = progressBar2.Value.ToString();
             }
             numericUpDown1.Value = progressBar1.Value;
             textBox1.Text = numericUpDown1.Value.ToString();
-
+            */
         }
         
         private void TextBox1_TextChanged(object sender, EventArgs e)
         {
-            if (!(Int32.Parse(textBox1.Text) > Int32.Parse(textBox2.Text)))
+            /*if (!(Convert.ToInt32(textBox1.Text) > Convert.ToInt32(textBox2.Text)))
             {
                 textBox2.Text = (Int32.Parse(textBox1.Text) - 1).ToString();
                 progressBar2.Value = Int32.Parse(textBox2.Text);
@@ -59,7 +59,7 @@ namespace WindowsFormsApp4
                 
             }
             progressBar1.Value = Int32.Parse(textBox1.Text);
-            numericUpDown1.Value = Int32.Parse(textBox1.Text);
+            numericUpDown1.Value = Int32.Parse(textBox1.Text);*/
         }
 
         private void NumericUpDown2_ValueChanged(object sender, EventArgs e)
@@ -80,20 +80,20 @@ namespace WindowsFormsApp4
         }
 
         private void ProgressBar2_Click(object sender, EventArgs e)
-        {
+        {/*
             if (!(progressBar1.Value > progressBar2.Value))
             {
-                progressBar1.Value = Decimal.ToInt32(progressBar2.Value + 1);
+                progressBar1.Value = progressBar2.Value + 1;
                 numericUpDown1.Value = progressBar1.Value;
                 textBox1.Text = progressBar1.Value.ToString();
             }
             numericUpDown2.Value = progressBar2.Value;
-            textBox2.Text = numericUpDown2.Value.ToString();
-        }
+            textBox2.Text = numericUpDown2.Value.ToString();*/
+    }
 
-        private void TextBox2_TextChanged(object sender, EventArgs e)
+    private void TextBox2_TextChanged(object sender, EventArgs e)
         {
-            if (!(Int32.Parse(textBox1.Text) > Int32.Parse(textBox2.Text)))
+          /*  if (!(Convert.ToInt32(textBox1.Text) > Convert.ToInt32(textBox2.Text)))
             {
                 textBox1.Text = (Int32.Parse(textBox2.Text) + 1).ToString();
                 progressBar1.Value = Int32.Parse(textBox1.Text);
@@ -101,7 +101,47 @@ namespace WindowsFormsApp4
 
             }
             progressBar2.Value = Int32.Parse(textBox2.Text);
-            numericUpDown2.Value = Int32.Parse(textBox2.Text);
+            numericUpDown2.Value = Int32.Parse(textBox2.Text);*/
+        }
+
+        private void TextBox2_TabStopChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int i1 = Convert.ToInt32(textBox1.Text);
+            int i2 = Int32.Parse(textBox2.Text);
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (!(i1 > i2))
+                {
+                    textBox2.Text = (Int32.Parse(textBox1.Text) - 1).ToString();
+                    progressBar2.Value = Int32.Parse(textBox2.Text);
+                    numericUpDown2.Value = Int32.Parse(textBox2.Text);
+
+                }
+                progressBar1.Value = Int32.Parse(textBox1.Text); 
+                numericUpDown1.Value = Int32.Parse(textBox1.Text);
+            }
+        }
+
+        private void TextBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (!(Convert.ToInt32(textBox1.Text) > Convert.ToInt32(textBox2.Text)))
+                {
+                    textBox1.Text = (Int32.Parse(textBox2.Text) + 1).ToString();
+                    progressBar1.Value = Int32.Parse(textBox1.Text);
+                    numericUpDown1.Value = Int32.Parse(textBox1.Text);
+
+                }
+                progressBar2.Value = Int32.Parse(textBox2.Text);
+                numericUpDown2.Value = Int32.Parse(textBox2.Text);
+            }
         }
 
         /* public class numer
